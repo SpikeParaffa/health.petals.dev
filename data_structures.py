44d4d2b756cd80cbf4d08829166272dcb02eac12
Spike Paraffa <spikeparaffa@gmail.com>
2024-09-20 10:15:58 +0900

    Support pydantic v2 fix

--- data_structures.py
+++ data_structures.py
@@ -1,11 +1,10 @@
 from typing import Optional
 from urllib.parse import urlparse
-
 import petals
-import pydantic
+from pydantic import BaseModel, Field
+from pydantic.v1 import dataclasses as pydantic_v1_dataclasses
 
-
-@pydantic.dataclasses.dataclass
+@pydantic_v1_dataclasses.dataclass
 class ModelInfo(petals.data_structures.ModelInfo):
     dht_prefix: Optional[str] = None
     official: bool = True
@@ -18,3 +17,29 @@ class ModelInfo(petals.data_structures.ModelInfo):
     @property
     def short_name(self) -> str:
         return self.name.split("/")[-1]
+
+class ModelInfoV2(BaseModel):
+    model: ModelInfo
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    @property
+    def dht_prefix(self) -> Optional[str]:
+        return self.model.dht_prefix
+
+    @property
+    def official(self) -> bool:
+        return self.model.official
+
+    @property
+    def limited(self) -> bool:
+        return self.model.limited
+
+    @property
+    def name(self) -> str:
+        return self.model.name
+
+    @property
+    def short_name(self) -> str:
+        return self.model.short_name
